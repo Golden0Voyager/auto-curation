209566ff0365f66e067b0818e64da5e434dfd03c
Haining <yhn0535@gmail.com>
2026-05-24 10:45:01 +0800

    feat: add Hammer Museum parser (Los Angeles)
    新增 Hammer Museum（Los Angeles）解析器。

---
+++ src/sites/hammer_museum.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class HammerMuseumParser(BaseSiteParser):
+    """Hammer Museum - UCLA.
+
+    UCLA 哈默博物馆，以当代艺术展览和公共项目著称。
+    """
+    source = "Hammer Museum"
+    city = "Los Angeles"
+    parser_key = "hammer_museum"
+    institution_type = "museum"
+    list_url = "https://hammer.ucla.edu/exhibitions"
+    link_patterns = [
+        r"hammer\.ucla\.edu/exhibitions/[^/]+",
+    ]
