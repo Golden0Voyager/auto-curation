6df955a354517410a1232e32461010545c3e1bc9
Haining <yhn0535@gmail.com>
2026-05-24 10:45:02 +0800

    feat: add Museum Ludwig parser (Cologne)
    新增 Museum Ludwig（Cologne）解析器。

---
+++ src/sites/museum_ludwig.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class MuseumLudwigParser(BaseSiteParser):
+    """Museum Ludwig - Cologne.
+
+    路德维希博物馆，科隆最重要的现代艺术博物馆，以毕加索和波普艺术收藏闻名。
+    """
+    source = "Museum Ludwig"
+    city = "Cologne"
+    parser_key = "museum_ludwig"
+    institution_type = "museum"
+    list_url = "https://www.museum-ludwig.de/en/home/exhibitions"
+    link_patterns = [
+        r"museum-ludwig\.de/en/home/exhibitions/[^/]+",
+    ]
