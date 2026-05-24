ada00b367c94a8856a5870da66cf075493463c38
Haining <yhn0535@gmail.com>
2026-05-24 10:45:02 +0800

    feat: add MCA Chicago parser (Chicago)
    新增 MCA Chicago（Chicago）解析器。

---
+++ src/sites/mca_chicago.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class MCAChicagoParser(BaseSiteParser):
+    """Museum of Contemporary Art Chicago.
+
+    芝加哥当代艺术博物馆，美国最重要的当代艺术机构之一。
+    """
+    source = "MCA Chicago"
+    city = "Chicago"
+    parser_key = "mca_chicago"
+    institution_type = "museum"
+    list_url = "https://mcachicago.org/exhibitions"
+    link_patterns = [
+        r"mcachicago\.org/exhibitions/.+",
+    ]
