da2f68d971bc31e84aedd14fb3a15e51cf214f14
Haining <yhn0535@gmail.com>
2026-05-24 10:43:05 +0800

    feat: add Taipei Biennial parser (Taipei) [双年展]
    新增 Taipei Biennial（Taipei）解析器。

---
+++ src/sites/taipei_biennale.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class TaipeiBiennialParser(BaseSiteParser):
+    """Taipei Biennial.
+
+    台北双年展，由台北市立美术馆主办，亚洲重要的当代艺术双年展。
+    """
+    source = "Taipei Biennial"
+    city = "Taipei"
+    parser_key = "taipei_biennale"
+    institution_type = "biennial"
+    list_url = "https://www.tfam.museum/Exhibition/Exhibition.aspx?ddlLang=en"
+    link_patterns = [
+        r"tfam\.museum/Exhibition/ExhibitionDetail\.aspx\?[^\"]+",
+    ]
