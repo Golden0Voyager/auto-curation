1a60c6756e492ab8fb6940eed2a968637cab9574
Haining <yhn0535@gmail.com>
2026-05-24 10:43:05 +0800

    feat: add Documenta parser (Kassel)
    新增 Documenta（Kassel）解析器。

---
+++ src/sites/documenta.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class DocumentaParser(BaseSiteParser):
+    """Documenta - Kassel.
+
+    德国卡塞尔文献展，当代艺术的顶级风向标，五年一届。
+    """
+    source = "Documenta"
+    city = "Kassel"
+    parser_key = "documenta"
+    institution_type = "quintennial"
+    list_url = "https://documenta.de/en"
+    link_patterns = [
+        r"documenta\.de/en/(retrospective|programme)/[^/]+",
+    ]
