5aafe3f705416ad551a4c82c2d8513c146211bb7
Haining <yhn0535@gmail.com>
2026-05-24 10:45:02 +0800

    feat: add NGV parser (Melbourne)
    新增 NGV（Melbourne）解析器。

---
+++ src/sites/ngv.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class NGVParser(BaseSiteParser):
+    """National Gallery of Victoria - Melbourne.
+
+    维多利亚州国立美术馆，澳大利亚历史最悠久、规模最大的美术馆。
+    """
+    source = "NGV"
+    city = "Melbourne"
+    parser_key = "ngv"
+    institution_type = "museum"
+    list_url = "https://www.ngv.vic.gov.au/whats-on/exhibitions/"
+    link_patterns = [
+        r"ngv\.vic\.gov\.au/whats-on/exhibitions/[^/]+",
+    ]
