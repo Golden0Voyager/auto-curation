5aab88c75bd3cd0a11afbbe7dc420ccd502b16ef
Haining <yhn0535@gmail.com>
2026-05-24 10:44:26 +0800

    feat: add Liverpool Biennial parser (Liverpool) [双年展]
    新增 Liverpool Biennial（利物浦）解析器。

---
+++ src/sites/liverpool_biennial.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class LiverpoolBiennialParser(BaseSiteParser):
+    """Liverpool Biennial.
+
+    英国利物浦双年展，英国最大的当代视觉艺术节。
+    """
+    source = "Liverpool Biennial"
+    city = "Liverpool"
+    parser_key = "liverpool_biennial"
+    institution_type = "biennial"
+    list_url = "https://www.biennial.com"
+    link_patterns = [
+        r"biennial\.com/[^/]+",
+    ]
