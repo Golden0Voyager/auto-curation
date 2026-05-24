39e68ca8e217b862cbfe36537aef719ea5c44efb
Haining <yhn0535@gmail.com>
2026-05-24 10:45:02 +0800

    feat: add Lenbachhaus parser (Munich)
    新增 Lenbachhaus（Munich）解析器。

---
+++ src/sites/lenbachhaus.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class LenbachhausParser(BaseSiteParser):
+    """Lenbachhaus - Munich.
+
+    伦巴赫之家，慕尼黑著名的市立美术馆，以青骑士派收藏闻名。
+    """
+    source = "Lenbachhaus"
+    city = "Munich"
+    parser_key = "lenbachhaus"
+    institution_type = "museum"
+    list_url = "https://www.lenbachhaus.de/en/program/exhibitions"
+    link_patterns = [
+        r"lenbachhaus\.de/en/program/exhibitions/details/[^/]+",
+    ]
