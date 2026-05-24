a356bf4d76bcfa7052c742471d5f2cdfddfd64c2
Haining <yhn0535@gmail.com>
2026-05-24 10:45:02 +0800

    feat: add MCA Australia parser (Sydney)
    新增 MCA Australia（Sydney）解析器。

---
+++ src/sites/mca_australia.py
@@ -0,0 +1,16 @@
+from src.sites.base import BaseSiteParser
+
+class MCAAustraliaParser(BaseSiteParser):
+    """Museum of Contemporary Art Australia - Sydney.
+
+    澳大利亚当代艺术博物馆，悉尼环形码头的标志性艺术机构。
+    注意：网站为 SPA，静态 HTML 无展览链接，需要 Playwright 渲染。
+    """
+    source = "MCA Australia"
+    city = "Sydney"
+    parser_key = "mca_australia"
+    institution_type = "museum"
+    list_url = "https://www.mca.com.au/exhibitions/"
+    link_patterns = [
+        r"mca\.com\.au/exhibitions/[^/]+",
+    ]
