8aae809257c1ae3cad8154a5fc010cd87b3270b6
Haining <yhn0535@gmail.com>
2026-05-24 10:45:01 +0800

    feat: add Fondation Beyeler parser (Basel)
    新增 Fondation Beyeler（Basel）解析器。

---
+++ src/sites/beyeler.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class BeyelerParser(BaseSiteParser):
+    """Fondation Beyeler - Basel.
+
+    贝耶勒基金会，瑞士最著名的私人美术馆之一。
+    """
+    source = "Fondation Beyeler"
+    city = "Basel"
+    parser_key = "beyeler"
+    institution_type = "museum"
+    list_url = "https://www.fondationbeyeler.ch/en/exhibitions"
+    link_patterns = [
+        r"fondationbeyeler\.ch/en/exhibitions/[^/]+",
+    ]
