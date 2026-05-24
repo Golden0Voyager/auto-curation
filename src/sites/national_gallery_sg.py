fac2faddcc3048c35a8e1880bfebc01329adf4a7
Haining <yhn0535@gmail.com>
2026-05-24 10:45:02 +0800

    feat: add National Gallery Singapore parser (Singapore)
    新增 National Gallery Singapore（Singapore）解析器。

---
+++ src/sites/national_gallery_sg.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class NationalGallerySGParser(BaseSiteParser):
+    """National Gallery Singapore.
+
+    新加坡国家美术馆，东南亚最大的现代艺术博物馆。
+    """
+    source = "National Gallery Singapore"
+    city = "Singapore"
+    parser_key = "national_gallery_sg"
+    institution_type = "museum"
+    list_url = "https://www.nationalgallery.sg/sg/en/whats-on.html"
+    link_patterns = [
+        r"nationalgallery\.sg/sg/en/exhibitions/[^/]+\.html",
+    ]
