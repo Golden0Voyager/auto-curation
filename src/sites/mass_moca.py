d21db6433b1ab33036faa7defaf180b4855aeb3c
Haining <yhn0535@gmail.com>
2026-05-24 10:45:02 +0800

    feat: add MASS MoCA parser (North Adams)
    新增 MASS MoCA（North Adams）解析器。

---
+++ src/sites/mass_moca.py
@@ -0,0 +1,17 @@
+from src.sites.base import BaseSiteParser
+
+class MASSMocaParser(BaseSiteParser):
+    """MASS MoCA.
+
+    马萨诸塞州当代艺术博物馆，以大型装置和特定场域作品闻名。
+    注意：受 Cloudflare 403 保护，需要 Playwright 或 stealth headers。
+    """
+    source = "MASS MoCA"
+    city = "North Adams"
+    parser_key = "mass_moca"
+    institution_type = "museum"
+    list_url = "https://massmoca.org/exhibitions/"
+    link_patterns = [
+        r"massmoca\.org/event/[^/]+",
+        r"massmoca\.org/exhibitions/[^/]+",
+    ]
