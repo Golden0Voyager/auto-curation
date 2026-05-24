3b0e38c24ec60d859693fc5fdda37dc2489bb67a
Haining <yhn0535@gmail.com>
2026-05-24 10:45:02 +0800

    feat: add MAIIAM parser (Chiang Mai)
    新增 MAIIAM（Chiang Mai）解析器。

---
+++ src/sites/maiiam.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class MAIIAMParser(BaseSiteParser):
+    """MAIIAM Contemporary Art Museum - Chiang Mai.
+
+    MAIIAM 当代艺术博物馆，泰国清迈的重要私人当代艺术馆。
+    """
+    source = "MAIIAM"
+    city = "Chiang Mai"
+    parser_key = "maiiam"
+    institution_type = "museum"
+    list_url = "https://maiiam.com/en/exhibition"
+    link_patterns = [
+        r"maiiam\.com/en/exhibition/[^/]+",
+    ]
