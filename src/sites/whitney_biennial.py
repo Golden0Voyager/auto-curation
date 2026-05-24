4058233d758a1f5f8341ec4243291af8e5858f63
Haining <yhn0535@gmail.com>
2026-05-24 10:43:05 +0800

    feat: add Whitney Biennial parser (New York) [双年展]
    新增 Whitney Biennial（New York）解析器。

---
+++ src/sites/whitney_biennial.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class WhitneyBiennialParser(BaseSiteParser):
+    """Whitney Biennial.
+
+    惠特尼双年展，美国当代艺术最具影响力的周期性展览。
+    """
+    source = "Whitney Biennial"
+    city = "New York"
+    parser_key = "whitney_biennial"
+    institution_type = "biennial"
+    list_url = "https://whitney.org/whitney-biennial"
+    link_patterns = [
+        r"whitney\.org/whitney-biennial/[^/]+",
+    ]
