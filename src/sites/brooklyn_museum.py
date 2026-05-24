8622969a3850032fe300c352f1b97661abc32033
Haining <yhn0535@gmail.com>
2026-05-24 10:45:01 +0800

    feat: add Brooklyn Museum parser (New York)
    新增 Brooklyn Museum（New York）解析器。

---
+++ src/sites/brooklyn_museum.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class BrooklynMuseumParser(BaseSiteParser):
+    """Brooklyn Museum.
+
+    布鲁克林博物馆，纽约市藏品最丰富的博物馆之一。
+    """
+    source = "Brooklyn Museum"
+    city = "New York"
+    parser_key = "brooklyn_museum"
+    institution_type = "museum"
+    list_url = "https://www.brooklynmuseum.org/exhibitions"
+    link_patterns = [
+        r"brooklynmuseum\.org/exhibitions/[^/]+",
+    ]
