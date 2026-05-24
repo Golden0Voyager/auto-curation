042202d03f61448e24be554e157b3801a43d3700
Haining <yhn0535@gmail.com>
2026-05-24 10:45:02 +0800

    feat: add Louisiana Museum parser (Humlebæk)
    新增 Louisiana Museum（Humlebæk）解析器。

---
+++ src/sites/louisiana.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class LouisianaParser(BaseSiteParser):
+    """Louisiana Museum of Modern Art.
+
+    路易斯安那现代艺术博物馆，丹麦最著名的当代艺术博物馆。
+    """
+    source = "Louisiana Museum"
+    city = "Humlebæk"
+    parser_key = "louisiana"
+    institution_type = "museum"
+    list_url = "https://louisiana.dk/en/exhibition/"
+    link_patterns = [
+        r"louisiana\.dk/en/exhibition/[^/]+",
+    ]
