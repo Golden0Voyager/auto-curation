3ea81ed61295441fc6307da754f58a1e0780c8b1
Haining <yhn0535@gmail.com>
2026-05-24 10:45:02 +0800

    feat: add South London Gallery parser (London)
    新增 South London Gallery（London）解析器。

---
+++ src/sites/south_london_gallery.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class SouthLondonGalleryParser(BaseSiteParser):
+    """South London Gallery.
+
+    南伦敦画廊，英国历史最悠久的当代艺术空间之一。
+    """
+    source = "South London Gallery"
+    city = "London"
+    parser_key = "south_london_gallery"
+    institution_type = "museum"
+    list_url = "https://www.southlondongallery.org/"
+    link_patterns = [
+        r"southlondongallery\.org/exhibitions/[^/]+",
+    ]
