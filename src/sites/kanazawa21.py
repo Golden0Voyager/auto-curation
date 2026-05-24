45448a8f2836d769409a72f36d8d7ca02f722ab7
Haining <yhn0535@gmail.com>
2026-05-24 10:45:01 +0800

    feat: add 21st Century Museum parser (Kanazawa)
    新增 21st Century Museum（Kanazawa）解析器。

---
+++ src/sites/kanazawa21.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class Kanazawa21Parser(BaseSiteParser):
+    """21st Century Museum of Contemporary Art, Kanazawa.
+
+    金泽21世纪美术馆，日本最具创新性的当代艺术博物馆之一。
+    """
+    source = "21st Century Museum"
+    city = "Kanazawa"
+    parser_key = "kanazawa21"
+    institution_type = "museum"
+    list_url = "https://www.kanazawa21.jp/en/exhibitions/"
+    link_patterns = [
+        r"kanazawa21\.jp/en/exhibitions/[^/]+",
+    ]
