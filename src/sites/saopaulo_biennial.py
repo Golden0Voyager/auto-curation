97c79c9a0cfc44502c9b350fec71e57931024f30
Haining <yhn0535@gmail.com>
2026-05-24 10:43:06 +0800

    feat: add São Paulo Biennial parser (São Paulo) [双年展]
    新增 São Paulo Biennial（São Paulo）解析器。

---
+++ src/sites/saopaulo_biennial.py
@@ -0,0 +1,15 @@
+from src.sites.base import BaseSiteParser
+
+class SaoPauloBiennialParser(BaseSiteParser):
+    """São Paulo Biennial.
+
+    圣保罗双年展，拉丁美洲历史最悠久的国际当代艺术双年展。
+    """
+    source = "São Paulo Biennial"
+    city = "São Paulo"
+    parser_key = "saopaulo_biennial"
+    institution_type = "biennial"
+    list_url = "https://www.bienal.org.br"
+    link_patterns = [
+        r"bienal\.org\.br/[^/]+",
+    ]
